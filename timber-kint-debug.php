a3c41ca5d055f50b78920b3a5c74087fc2ca0308
Sascha Paukner <sascha@landwire.net>
2020-10-19 12:47:56 +0200

    added admin check

--- timber-kint-debug.php
+++ timber-kint-debug.php
@@ -18,7 +18,7 @@ class Timber_Kint_Debug {
     return $twig;
   }
   function call_kint($twig, $vars, $context = FALSE) {
-    if ($twig->isDebug()) {
+    if ($twig->isDebug() && current_user_can('administrator')) {
       if (!class_exists('Kint')) {
         return 'Kint class doesn\'t exist! You can download it from <a href="https://wordpress.org/plugins/kint-debugger/">https://wordpress.org/plugins/kint-debugger/</a>';
       }
